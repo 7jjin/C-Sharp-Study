16327e4720a8d30b5fe3bca6b5402e4ebc9830de
7jjin <wlsgud6305@naver.com>
2024-06-21 22:33:05 +0900

    Add files via upload

---
+++ IF_Study2/IF_Study2.cs
@@ -0,0 +1,27 @@
+﻿using MyLibrary;
+using System;
+namespace IF_Study2
+{
+    public class IF_Study2
+    {
+        public void Run()
+        {
+            //여기부터 프로그램 시작
+            PrintResult(Util.GetDigit());
+        }
+
+        private void PrintResult(int digit)
+        {
+            if (digit > 90)
+            {
+                Console.WriteLine("우수");
+            }
+            else
+            {
+                Console.WriteLine("보통");
+            }
+        }
+
+        
+    }
+}
---
+++ IF_Study2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_Study2
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IF_Study2 obj = new IF_Study2();
+            obj.Run();
+        }
+    }
+
+}
